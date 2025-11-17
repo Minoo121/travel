d4db2c07b829b3dd2d6d132151f49c33157c4af1
Minoo121 <minoow1120@gmail.com>
2025-11-17 23:10:20 +0900

    top.htmlを直下へ移動しました

--- script/rogin.js
+++ script/rogin.js
@@ -11,7 +11,7 @@ function pass_col() {
     input.value = toHankaku(input.value);
 
     if (pass == input.value) {
-        window.location.href = "../html/top.html"
+        window.location.href = "../top.html"
         return true;
     }
     error();
