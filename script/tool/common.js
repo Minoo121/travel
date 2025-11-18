ccdca26acb92f57bb52f58198e143173cfc383cc
Minoo121 <minoow1120@gmail.com>
2025-11-18 23:48:11 +0900

    オーバーレイを作成

---
+++ script/tool/common.js
@@ -0,0 +1,18 @@
+function openModel(){
+    let model = document.getElementById("model");
+    model.style.display = "flex";
+}
+
+function closeModel(){
+    let model = document.getElementById("model");
+    model.style.display = "none";
+}
+
+window.addEventListener("click",
+    function(e){
+        let model = this.document.getElementById("model");
+        if(e.target === model){
+            closeModel();
+        }
+    }
+);
