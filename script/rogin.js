91077925801f473e61321a920037a27920c221a8
Minoo121 <minoow1120@gmail.com>
2025-11-18 18:06:55 +0900

    JSのワークスペースを整頓

--- script/rogin.js
+++ script/rogin.js
@@ -1,23 +1,7 @@
-document.getElementById("error").style.display = "none";
-
+//********************************************************* */
+//汎用
+//********************************************************* */
 const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));//timeはミリ秒
-
-function error() {
-  document.getElementById("error").style.display = "block";
-};
-
-async function pass_col() {
-  let pass = 250208;
-  let input = document.getElementById("pass");
-  input.value = toHankaku(input.value);
-  if (pass == input.value) {
-    document.getElementById("img").src="../img/unlock.png";
-    await sleep(1000);
-    window.location.href = "./top.html"
-    return true;
-  }
-  error();
-}
 
 function toHankaku(input) {
   // DOM 要素が渡されたら value を使う
@@ -28,16 +12,40 @@ function toHankaku(input) {
   if (input == null) input = '';
   // 最終的に文字列に変換
   const str = String(input);
-
+  
   // 全角英数字・記号（！-～）を半角へ
   let out = str.replace(/[！-～]/g, ch =>
     String.fromCharCode(ch.charCodeAt(0) - 0xFEE0)
   );
-
+  
   // 全角スペースを半角に
   out = out.replace(/\u3000/g, ' ');
-
+  
   return out;
+}
+//********************************************************* */
+//index.html
+//********************************************************* */
+document.getElementById("error").style.display = "none";
+
+function error() {
+  document.getElementById("error").style.display = "block";
+};
+
+async function pass_col() {
+  let pass = 250208;
+  let input = document.getElementById("pass");
+  input.value = toHankaku(input.value);
+  if (pass == input.value) {
+    document.getElementById("img").src = "../img/unlock.png";
+    await sleep(1000);
+    window.location.href = "./top.html"
+    return true;
+  }
+  error();
 }
 
 
+//********************************************************* */
+//top.html
+//********************************************************* */
